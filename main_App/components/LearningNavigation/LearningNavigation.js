4184f5eee56e67cfa8c955c12a92a8e4a9f027e1
maxim <maxim19990203@outlook.com>
2021-04-25 18:56:47 +0500

    Small refactor in learning-list component

--- main_App/components/LearningNavigation/LearningNavigation.js
+++ main_App/components/LearningNavigation/LearningNavigation.js
@@ -6,11 +6,17 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function LearningNavigation ({ onNavigateToPage}){
  return (
-     <div className='main-learning-menu'>
-         <ul className='learning-menu' onClick={(event) => onNavigateToPage(event.target.id)}>
-         <li><Link id={Page.learningSharp} className='learning-ref' to="/learning/sharp">Обучение C#</Link></li>
-         <li><Link id={Page.learningJS} className='learning-ref' to="/learning/js">Обучение JS</Link></li>
-         <li><Link id={Page.learningSQL} className='learning-ref' to="/learning/sql">Обучение SQL</Link></li>
+     <div className='learning-menu'>
+        <ul className='learning-menu-list' onClick={(event) => onNavigateToPage(event.target.id)}>
+            <li className='learning-menu-list__item'>
+                <Link id={Page.learningSharp} className='learning-ref' to="/learning/sharp">Обучение C#</Link>
+            </li>
+            <li className='learning-menu-list__item'>
+                <Link id={Page.learningJS} className='learning-ref' to="/learning/js">Обучение JS</Link>
+            </li>
+            <li className='learning-menu-list__item'>
+                <Link id={Page.learningSQL} className='learning-ref' to="/learning/sql">Обучение SQL</Link>
+            </li>
          </ul>
      </div>
  );
