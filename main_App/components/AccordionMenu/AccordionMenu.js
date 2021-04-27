e835693fa98f65455214df678cd7f1ddbe42130f
Vadim Korotkikh <vad-korotkikh-im-203311998@mail.ru>
2021-04-27 14:03:15 +0500

    add automatic AccordionMenu

--- main_App/components/AccordionMenu/AccordionMenu.js
+++ main_App/components/AccordionMenu/AccordionMenu.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { object } from 'prop-types';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Page from "../../constants/Page";
+import testAccordionMenu from "../../forTests/index"
 import './style.css';
 
 export default function AccordionMenu ({ onNavigateToPage }){
+  const accordionMenu = [];
+  const subMenu=[]
+  for (let el of testAccordionMenu){
+    accordionMenu.push(<div key={el.id} className='element' onClick={()=>{document.getElementById(el.sectionName).classList.toggle('open-sub-menu') }}>
+    <a  className='elem-title'>{el.sectionName}</a>
+    <div className='sub-menu' id={el.sectionName}>
+      {el.articles.map(function (obj,i){
+        return <a key={obj.id} onClick={(event)=>event.stopPropagation()}>{obj.name}</a>
+      }) }
+    </div>
+    </div>) 
+  }
     return (
       <div className='main-learning-container'>
         <div className='learning-container'>
@@ -12,29 +25,7 @@ export default function AccordionMenu ({ onNavigateToPage }){
             <Link className='back-ref' to ='/learning'>
           <div onClick= {()=>onNavigateToPage(Page.learningMenu)} className='button-back'>&lArr; Вернуться</div>
           </Link>
-            <div className='element' onClick={()=>{document.getElementById('test').classList.toggle('open-sub-menu') }}>
-              <a  className='elem-title'>Начальный уровень</a>
-              <div className='sub-menu' id='test'>
-                <a onClick={(event)=>event.stopPropagation()}>Тест 1</a>
-                <a onClick={(event)=>event.stopPropagation()}>Тест 2</a>
-              </div>
-              </div>
-
-              <div className='element' onClick={()=>{document.getElementById('test1').classList.toggle('open-sub-menu') }}>
-              <a className='elem-title'>Средний уровень</a>
-              <div className='sub-menu' id='test1'>
-                <a onClick={(event)=>event.stopPropagation()}>Другой тест 1</a>
-                <a onClick={(event)=>event.stopPropagation()}>Другой тест 2</a>
-                <a onClick={(event)=>event.stopPropagation()}>Другой тест 2</a>
-              </div>
-              </div>
-              <div className='element' onClick={()=>{document.getElementById('test2').classList.toggle('open-sub-menu') }}>
-              <a className='elem-title'>Последний уровень</a>
-              <div className='sub-menu' id='test2'>
-                <a onClick={(event)=>event.stopPropagation()}>Другой тест 1</a>
-                <a onClick={(event)=>event.stopPropagation()}>Другой тест 2</a>
-              </div>
-              </div>
+            {accordionMenu}
           </div>
         </div>
         <div className='learning-content'>
